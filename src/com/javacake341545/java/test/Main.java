f922a664613ec8d4acfc1f111bb447dcccc70980
javacake341545 <javacakeman777@gmail.com>
2023-02-01 14:46:29 +0900

    스캐너로 명령어 입력받기

---
+++ src/com/javacake341545/java/test/Main.java
@@ -0,0 +1,22 @@
+package com.javacake341545.java.test;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("==program starts==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("input ) ");
+		String cmd = sc.nextLine(); // string 타입의 명령어 // line : 입력된 모든 문자가 입력됨.
+//		String cmd = sc.next(); 띄어쓰기 못씀.
+		
+		System.out.println("command : " + cmd);
+		
+		System.out.println("==program ends==");
+		
+		sc.close();
+	}
+
+}
