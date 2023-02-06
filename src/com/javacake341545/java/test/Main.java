25762b9f5885342a1cf174e5fb2ef2f95f2d8622
javacake341545 <javacakeman777@gmail.com>
2023-02-06 14:59:37 +0900

    ariticle mod

--- src/com/javacake341545/java/test/Main.java
+++ src/com/javacake341545/java/test/Main.java
@@ -53,11 +53,12 @@ public class Main {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
-				Article article = new Article(id, regDate, title, body);
+				Article article = new Article(id, regDate, regDate, title, body);
 				articles.add(article);
 
 				System.out.printf("%d번 글이 생성 되었습니다\n", id);
 				lastArticleId++;
+				
 			} else if (command.startsWith("article detail ")) {
 				String[] commandBits = command.split(" ");
 
@@ -80,11 +81,48 @@ public class Main {
 				}
 				System.out.println(id + "번 글은 존재합니다.");
 				System.out.printf("번호 : %d\n", foundArticle.id);
-				System.out.printf("날짜 : %s\n", foundArticle.regDate);
+				System.out.printf("작성날짜 : %s\n", foundArticle.regDate);
+				System.out.printf("수정날짜 : %s\n", foundArticle.updateDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 				
 			} else if (command.startsWith("article delete ")) {
+				String[] commandBits = command.split(" ");
+
+				int id = Integer.parseInt(commandBits[2]);
+
+				Article foundArticle = null;
+				
+				// int foundIndex = -1;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+//					if (article.id == id) {
+//						foundIndex = i;
+//						break;
+					}
+				}
+//				if (foundIndex == -1) {
+//					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+//					continue;
+					
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				//size() -> 3이라면
+				//index : 0, 1, 2
+				//id : 1, 2, 3
+//				articles.remove(id); id는 1인데 index는 0이기에 오류 발생
+				//articles.remove(id - 1); // 을 해야 인덱스와 아이디가 동일해서 제거 가능
+				//하지만 지운 후 다시 쓴 글이 id 1로 들어감. 문제 발생.
+				System.out.println(id + "번 글을 삭제합니다");
+				articles.remove(foundArticle);
+				
+			} else if (command.startsWith("article modify ")) {
 				String[] commandBits = command.split(" ");
 
 				int id = Integer.parseInt(commandBits[2]);
@@ -98,19 +136,25 @@ public class Main {
 						break;
 					}
 				}
+				
+
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
-				//size() -> 3이라면
-				//index : 0, 1, 2
-				//id : 1, 2, 3
-//				articles.remove(id); id는 1인데 index는 0이기에 오류 발생
-				//articles.remove(id - 1); // 을 해야 인덱스와 아이디가 동일해서 제거 가능
-				//하지만 지운 후 다시 쓴 글이 id 1로 들어감. 문제 발생.
-				System.out.println(id + "번 글을 삭제합니다");
-				articles.remove(foundArticle);
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				String updateDate = Util.getNowDateStr();
+
+				foundArticle.title = title;
+				foundArticle.body = body;
+				foundArticle.updateDate = updateDate;
+
+				System.out.println(id + "번 글이 수정되었습니다");
 			}
+				
 
 			else {
 				System.out.println("존재하지 않는 명령어입니다");
@@ -129,6 +173,7 @@ public class Main {
 class Article {
 	int id;
 	String regDate;
+	String updateDate = "";
 	String title;
 	String body;
 
