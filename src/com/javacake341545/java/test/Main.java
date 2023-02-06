55b2317e729e27379e5acd3f8e06b2ded319817e
javacake341545 <javacakeman777@gmail.com>
2023-02-06 14:22:26 +0900

    article del

--- src/com/javacake341545/java/test/Main.java
+++ src/com/javacake341545/java/test/Main.java
@@ -1,30 +1,142 @@
 package com.javacake341545.java.test;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		System.out.println("==program starts==");
-		
-		Scanner sc = new Scanner(System.in);
-		
-		
-		while(true) { // 무한대 실행
-			System.out.println("input ) ");
-			String cmd = sc.nextLine(); // string 타입의 명령어 // line : 입력된 모든 문자가 입력됨.
-//		String cmd = sc.next(); 띄어쓰기 못씀.
+		System.out.println("==프로그램 시작==");
 
-			System.out.println("command : " + cmd);
-			
-			if(cmd.equals("system exit")) {
+		Scanner sc = new Scanner(System.in);
+
+		int lastArticleId = 0;
+
+		List<Article> articles = new ArrayList<>();
+
+		while (true) {
+
+			System.out.printf("명령어 ) ");
+			String command = sc.nextLine().trim();
+
+			if (command.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
+			if (command.equals("system exit")) {
 				break;
 			}
+
+			if (command.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
+				System.out.println("번호    /     제목    ");
+				String tempTitle = null;
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					if (article.title.length() > 4) {
+						tempTitle = article.title.substring(0, 4);
+						System.out.printf("%d	/	%s\n", article.id, tempTitle + "...");
+						continue;
+					}
+
+					System.out.printf("%d	/	%s\n", article.id, article.title);
+				}
+			} else if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				System.out.printf("제목 : ");
+				String regDate = Util.getNowDateStr();
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				Article article = new Article(id, regDate, title, body);
+				articles.add(article);
+
+				System.out.printf("%d번 글이 생성 되었습니다\n", id);
+				lastArticleId++;
+			} else if (command.startsWith("article detail ")) {
+				String[] commandBits = command.split(" ");
+
+				int id = Integer.parseInt(commandBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				System.out.println(id + "번 글은 존재합니다.");
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", foundArticle.regDate);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+				
+			} else if (command.startsWith("article delete ")) {
+				String[] commandBits = command.split(" ");
+
+				int id = Integer.parseInt(commandBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				//size() -> 3이라면
+				//index : 0, 1, 2
+				//id : 1, 2, 3
+//				articles.remove(id); id는 1인데 index는 0이기에 오류 발생
+				//articles.remove(id - 1); // 을 해야 인덱스와 아이디가 동일해서 제거 가능
+				//하지만 지운 후 다시 쓴 글이 id 1로 들어감. 문제 발생.
+				System.out.println(id + "번 글을 삭제합니다");
+				articles.remove(foundArticle);
+			}
+
+			else {
+				System.out.println("존재하지 않는 명령어입니다");
+			}
+
 		}
-		
-		
-		System.out.println("==program ends==");
-		
+
+		System.out.println("==프로그램 끝==");
+
 		sc.close();
 	}
 
+
+}
+
+class Article {
+	int id;
+	String regDate;
+	String title;
+	String body;
+
+	Article(int id, String regDate, String title, String body) {
+		this.id = id;
+		this.regDate = regDate;
+		this.title = title;
+		this.body = body;
+	}
+	
 }
---
+++ src/com/javacake341545/java/test/Util.java
@@ -0,0 +1,16 @@
+package com.javacake341545.java.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	/** 현재 날짜 시간 String */
+	public static String getNowDateStr() {
+
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		Date now = new Date();
+
+		return sdf1.format(now);
+	}
+}
